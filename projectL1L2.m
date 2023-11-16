e4aafbbcaa58c5aa1c59feaf4687ebcbc32a7968
htpusa <taneli.pusa@pm.me>
2023-11-16 10:25:20 +0200

    update l1l2projection

--- projectL1L2.m
+++ projectL1L2.m
@@ -1,17 +1,23 @@
 function xProj = projectL1L2(x,c)
 
-% L1L2-projection from [1].
+% PROJECTL1L2 L_1-L_2-projection from Witten et al. 2009
+%  xProj = projectL1L2(x,c)
+%
+%  EXAMPLE
+%  x = rand(10,1);
+%  xProj = projectL1L2(x,2);
 
-%   [1] Witten, Daniela M., Robert Tibshirani, and Trevor Hastie. 
+%  Witten, Daniela M., Robert Tibshirani, and Trevor Hastie. 
 %    "A penalized matrix decomposition, with applications to sparse 
 %    principal components and canonical correlation analysis." 
 %    Biostatistics 10.3 (2009): 515-534.
 
 convergenceCrit = 1e-5;
 maxIter = 100;
+nMax = sum(max(abs(x))==abs(x));
 
-if c<=1
-    [m i] = max(abs(x));
+if sqrt(nMax)>=c % c<=1 or pathological duplicate max values
+    [~,i] = max(abs(x));
     xProj = zeros(numel(x),1);
     xProj(i) = c*sign(x(i));
 else
